ec0ccde68539ff11c6da924967d670c28cc26eb3
lukabar20 <138611098+lukabar20@users.noreply.github.com>
2023-07-14 11:29:09 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -7,43 +7,57 @@ tvg_ids_logos = {'GOL MUNDIAL HD': 'https://i.postimg.cc/3ryZ9n8G/golmundial.png
                  'DAZN F1 HD': 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012018/untitled-1_20.png?An9Fa1zRO4z6Dj__EVR4da6YOWsvtEw2&itok=6PiLMTa5',
                  'DAZN LaLiga HD': 'https://i.ibb.co/d541mDr/DAZN-La-Liga.jpg',
                  'DAZN LaLiga 2 HD': 'https://i.ibb.co/d541mDr/DAZN-La-Liga.jpg',
-                 'EUROSPORT 1 HD': 'https://i.ibb.co/DKTwL0t/Eurosport1-o.jpg',
-                 'EUROSPORT 2': 'https://i.ibb.co/4dFxnrC/Eurosport2-o.jpg',
-                 'M+ #VAMOS HD': 'https://img.sport-tv-guide.live/images/tv-station-vamos-1601.png',
-                 'M+ DEPORTES HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
-                 'M+ DEPORTES 2 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-2-365.png',
-                 'M+ DEPORTES 3': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-3-2502.png',
-                 'M+ DEPORTES 4': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
-                 'M+ DEPORTES 5': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
-                 'M+ DEPORTES 6': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
-                 'M+ GOLF HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-golf-368.png',
-                 'M+ LaLigaTV HD': 'https://i.ibb.co/jVMhMQQ/Liga-Santander.jpg',
-                 'LaLiga SmartbankTV HD': 'https://i.postimg.cc/CKxJXggY/ligasmartbank.png',
-                 'LaLiga SmartbankTV M3 HD': 'https://i.postimg.cc/4NfkryTV/ligasmartbank3.png',
-                 'LaLiga SmartbankTV M2 HD': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
-                 'M+ LALIGATV BAR': 'https://i.ibb.co/ykCff9y/La-Liga-Bar.jpg',
+                 'DAZN LaLiga 3 HD': 'https://i.ibb.co/d541mDr/DAZN-La-Liga.jpg',
+                 'DAZN LaLiga 4 HD': 'https://i.ibb.co/d541mDr/DAZN-La-Liga.jpg',
+                 'DAZN LaLiga 5 HD': 'https://i.ibb.co/d541mDr/DAZN-La-Liga.jpg',
+                 'Eurosport 1 HD': 'https://i.ibb.co/DKTwL0t/Eurosport1-o.jpg',
+                 'Eurosport 2': 'https://i.ibb.co/4dFxnrC/Eurosport2-o.jpg',
+                 'M+ #Vamos HD': 'https://img.sport-tv-guide.live/images/tv-station-vamos-1601.png',
+                 'M+ Deportes HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
+                 'M+ Deportes 2 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-2-365.png',
+                 'M+ Deportes 3': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-3-2502.png',
+                 'M+ Deportes 4': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
+                 'M+ Deportes 5': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
+                 'M+ Deportes 6': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
+                 'M+ Deportes 7': 'https://img.sport-tv-guide.live/images/tv-station-movistar-deportes-364.png',
+                 'M+ Golf HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-golf-368.png',
+                 'M+ LaLiga TV HD': 'https://telegra.ph/file/87a4c3d008b086629d093.jpg',
+                 'M+ LaLiga TV 2 HD': 'https://telegra.ph/file/5c77da09724e53af96832.jpg',
+                 'M+ LaLiga TV 3 HD': 'https://telegra.ph/file/968c9d7eb40340bae2eab.jpg',
+                 'M+ LaLiga TV 4': 'https://telegra.ph/file/9706db24e007948767d6e.jpg',
+                 'M+ LaLiga TV 5': 'https://telegra.ph/file/597c0cdd125c9199ed9da.jpg',
+                 'M+ LaLiga TV 6': 'https://telegra.ph/file/6726d0894872da50d69d8.jpg',
+                 'LaLiga TV Hypermotion HD': 'https://i.postimg.cc/CKxJXggY/ligasmartbank.png',
+                 'LaLiga TV Hypermotion 3 HD': 'https://i.postimg.cc/4NfkryTV/ligasmartbank3.png',
+                 'LaLiga TV Hypermotion 2 HD': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV Hypermotion 4': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV Hypermotion 5': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV Hypermotion 6': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV Hypermotion 7': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV Hypermotion 8': 'https://i.postimg.cc/9FL5V2WS/ligasmartbank2.png',
+                 'LaLiga TV BAR HD': 'https://i.ibb.co/ykCff9y/La-Liga-Bar.jpg',
                  'M+ Liga de Campeones HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-1459.png',
+                 'M+ Liga de Campeones 2 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-2-1460.png',
+                 'M+ Liga de Campeones 3 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-3-1548.png',
+                 'M+ Liga de Campeones 4 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-4-1549.png',
+                 'M+ Liga de Campeones 5 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-5-1550.png',
+                 'M+ Liga de Campeones 6 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-6-1551.png',
                  'M+ Liga de Campeones 7 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-7-1552.png',
                  'M+ Liga de Campeones 8 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-8-1602.png',
                  'M+ Liga de Campeones 9 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-1459.png',
                  'M+ Liga de Campeones 10 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-1459.png',
                  'M+ Liga de Campeones 11 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-1459.png',
                  'M+ Liga de Campeones 12 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-1459.png',
-                 'M+ Liga de Campeones 2 HD': 'https://img.sport-tv-guide.live/images/tv-station-moviestar-liga-de-campeones-2-1460.png',
-                 'M+ Liga de Campeones 3 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-3-1548.png',
-                 'M+ Liga de Campeones 4 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-4-1549.png',
-                 'M+ Liga de Campeones 5 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-5-1550.png',
-                 'M+ Liga de Campeones 6 HD': 'https://img.sport-tv-guide.live/images/tv-station-movistar-liga-de-campeones-6-1551.png',
                  'M+ #0 HD': 'https://img.sport-tv-guide.live/images/stations/a371.png',
-                 'Barça TV': 'https://img.sport-tv-guide.live/images/tv-station-barca-tv-1456.png',
-                 'BE MAD': 'https://img.sport-tv-guide.live/images/tv-station-be-mad-621.png',
-                 'CUATRO HD': 'https://img.sport-tv-guide.live/images/tv-station-cuatro-619.png',
+                 'Barcelona TV': 'https://img.sport-tv-guide.live/images/tv-station-barca-tv-1456.png',
+                 'Be Mad': 'https://img.sport-tv-guide.live/images/tv-station-be-mad-621.png',
+                 'Cuatro HD': 'https://img.sport-tv-guide.live/images/tv-station-cuatro-619.png',
                  'GOL PLAY': 'https://i.ibb.co/5hYsXDR/Gol.jpg',
-                 'HISTORIA': 'https://i.postimg.cc/NGrXN3S1/historia.png',
+                 #'Historia': 'https://i.postimg.cc/NGrXN3S1/historia.png',
                  'LA 1 HD': 'https://img.sport-tv-guide.live/images/tv-station-la-1-838.png',
-                 'NAT GEO HD': 'https://i.postimg.cc/CMb7QnfM/natgeo.png',
-                 'TELECINCO HD': 'https://img.sport-tv-guide.live/images/stations/a907.png',
-                 'TELEDEPORTE': 'https://i.ibb.co/thh1tKH/TDP.jpg',
+                 #'Nat Geo HD': 'https://i.postimg.cc/CMb7QnfM/natgeo.png',
+                 'Telecinco HD': 'https://img.sport-tv-guide.live/images/stations/a907.png',
+                 'Teledeporte': 'https://i.ibb.co/thh1tKH/TDP.jpg',
                  'Sport TV1 POR HD': 'https://img2.sport-tv-guide.live/images/stations/a450.png',
                  'Sport TV2 POR HD': 'https://img2.sport-tv-guide.live/images/stations/a451.png',
                  'Sport TV3 POR HD': 'https://img2.sport-tv-guide.live/images/stations/a452.png',
@@ -65,7 +79,8 @@ tvg_ids_logos = {'GOL MUNDIAL HD': 'https://i.postimg.cc/3ryZ9n8G/golmundial.png
                  'SETANTA': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6gKALgnJ-yAuP-WEwXAioFZ98lGiaiOvaAh4JN4q8umxsMxiylU-meSp8jCuVfpcevcU&usqp=CAU',
                  'OTROS': 'https://i.imgur.com/U4w7Bgy.png'}
 
-group_title_order = ["DAZN F1", "GOL MUNDIAL", "GOL MUNDIAL 2", "manuK0S", "M+ LaLiga", "DAZN LaLiga", "M+ Champions", "DAZN", "M+ Deportes", "M+ LaLiga BAR", "M+ LaLiga Smartbank", "EuroSport", "Otros deportes", "Otros"]
+
+group_title_order = ["DAZN F1", "GOL MUNDIAL", "GOL MUNDIAL 2", "manuK0S", "M+ LaLiga", "DAZN LaLiga", "M+ Champions", "DAZN", "M+ Deportes", "M+ LaLiga BAR", "M+ LaLiga Smartbank", "EuroSport", "Otros deportes", "electroperra", "Otros"]
 
 
 def extract_group_title(channel_title):
@@ -93,11 +108,11 @@ def extract_group_title(channel_title):
         return "M+ LaLiga BAR"
     elif "CAMPEONES" in title:
         return "M+ Champions"
-    elif "MUNDIAL" in title:
-        if "2" in title:
-            return "GOL MUNDIAL 2"
-        else:
-            return "GOL MUNDIAL"
+    #elif "MUNDIAL" in title:
+        #if "2" in title:
+            #return "GOL MUNDIAL 2"
+        #else:
+            #return "GOL MUNDIAL"
     elif "GOL" in title or "TDP" in title or "TENNIS" in title or "SPORT" in title or "BARÇA" in title or "ESPN" in title or "SETENTA" in title or "EUROSPORT" in title:
         return "Otros deportes"
     else:
@@ -109,11 +124,11 @@ def extract_tvg_id(channel_title):
     title = channel_title.upper().replace("1080", "").replace("720", "")
 
     if "BARÇA" in title or "BARCA" in title or "BARÃ§A" in title:
-        return "Barça TV"
+        return "Barcelona TV"
     elif "BE MAD" in title or "BEMAD" in title:
-        return "BE MAD"
+        return "Be Mad"
     elif "CUATRO" in title:
-        return "CUATRO HD"
+        return "Cuatro HD"
     elif "DAZN" in title:
         if "DAZN 2" in title:
             return "DAZN 2 HD"
@@ -124,16 +139,22 @@ def extract_tvg_id(channel_title):
         elif "F1" in title or "FORMULA" in title or "FÓRMULA" in title:
             return 'DAZN F1 HD'
         elif "LIGA" in title:
-                        if "2" in title:
-                                return 'DAZN LaLiga 2 HD'
-                        else:
-                                return 'DAZN LaLiga HD'
+            if "2" in title:
+                return "DAZN LaLiga 2 HD"
+            elif "3" in title:
+                return "DAZN LaLiga 3 HD"
+            elif "4" in title:
+                return "DAZN LaLiga 4 HD"
+            elif "5" in title:
+                return "DAZN LaLiga 5 HD"
+            else:
+                return "DAZN LaLiga HD"
         else:
             return "DAZN 1 HD"
     elif "EUROSPORT 2" in title:
-        return "EUROSPORT 2"
+        return "Eurosport 2"
     elif "EUROSPORT" in title:
-        return "EUROSPORT 1 HD"
+        return "Eurosport 1 HD"
     elif "GOL" in title and "GOLF" not in title and "MUNDIAL" not in title:
         return "GOL PLAY"
     elif "MUNDIAL" in title:
@@ -142,49 +163,75 @@ def extract_tvg_id(channel_title):
         else:
             return "GOL MUNDIAL HD"
     elif "HISTORIA" in title:
-        return "HISTORIA"
+        return "Historia"
     elif "LA 1" in title or "LA1" in title:
-        return "LA 1 HD"
+        return "La 1 HD"
     elif "BAR" in title and "BARÇA" not in title:
-        return 'M+ LALIGATV BAR'
-    elif "SMARTBANK 2" in title or "SMARTBANK M2" in title:
-        return 'LaLiga SmartbankTV M2 HD'
-    elif "SMARTBANK 3" in title:
-        return 'LaLiga SmartbankTV M3 HD'
-    elif "SMARTBANK" in title or "SMARTBANCK" in title:
-        return 'LaLiga SmartbankTV HD'
+        return 'LaLiga TV BAR HD'
+    elif "SMARTBANK" in title:
+        if "2" in title:
+            return "LaLiga TV Hypermotion 2 HD"
+        elif "3" in title:
+            return "LaLiga TV Hypermotion 3 HD"
+        elif "4" in title:
+            return "LaLiga TV Hypermotion 4"
+        elif "5" in title:
+            return "LaLiga TV Hypermotion 5"
+        elif "6" in title:
+            return "LaLiga TV Hypermotion 6"
+        elif "7" in title:
+            return "LaLiga TV Hypermotion 7"
+        elif "8" in title:
+            return "LaLiga TV Hypermotion 8"
+        else:
+            return "LaLiga TV Hypermotion HD"
     elif "# 0" in title or "#0" in title:
         return 'M+ #0 HD'
     elif "VAMOS" in title:
-        return 'M+ #VAMOS HD'
+        return 'M+ #Vamos HD'
     elif "GOLF" in title:
-        return 'M+ GOLF HD'
+        return 'M+ Golf HD'
     elif "LIGA" in title and "BAR" not in title and "DAZN" not in title and "SMARTBANK" not in title and "CAMPEONES" not in title:
-        return "M+ LaLigaTV HD"
+        if "2" in title:
+            return "M+ LaLiga TV 2 HD"
+        elif "3" in title:
+            return "M+ LaLiga TV 3 HD"
+        elif "4" in title:
+            return "M+ LaLiga TV 4"
+        elif "5" in title:
+            return "M+ LaLiga TV 5"
+        elif "6" in title:
+            return "M+ LaLiga TV 6"
+        else:
+            return "M+ LaLiga TV HD"
     elif "DEPORTES" in title:
         if "2" in title:
-            return "M+ DEPORTES 2 HD"
+            return "M+ Deportes 2 HD"
         elif "3" in title:
-            return "M+ DEPORTES 3"
+            return "M+ Deportes 3"
         elif "4" in title:
-            return "M+ DEPORTES 4"
+            return "M+ Deportes 4"
         elif "5" in title:
-            return "M+ DEPORTES 5"
+            return "M+ Deportes 5"
         elif "6" in title:
-            return "M+ DEPORTES 6"
+            return "M+ Deportes 6"
+        elif "7" in title:
+            return "M+ Deportes 7"
         else:
-            return "M+ DEPORTES HD"
+            return "M+ Deportes HD"
     elif "CAMPEONES" in title:
         for i in ["12", "3", "4", "5", "6", "7", "8", "9", "10", "11", "2"]:
             if i in title:
                 return "M+ Liga de Campeones " + i +" HD"
         return "M+ Liga de Campeones HD"
     elif "NATIONAL" in title or "GEOGRAPHIC" in title or "NAT" in title or "GEO" in title:
-        return 'NAT GEO HD'
+        return 'National Geographic HD'
+    elif "WILD" in title or "NATIONAL" in title or "GEOGRAPHIC" in title or "NAT" in title or "GEO" in title:
+        return 'Nat Geo Wild HD'
     elif "CINCO" in title or "CIRCO"in title:
-        return "TELECINCO HD"
+        return "Telecinco HD"
     elif "TELEDEPORTE" in title or "TDP" in title:
-        return "TELEDEPORTE"
+        return "Teledeporte"
     elif "SPORT TV 1" in title:
         return "Sport TV1 POR HD"
     elif "SPORT TV 2" in title:
